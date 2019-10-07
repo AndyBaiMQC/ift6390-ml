fece5921ba52fd9fd3221c04396ab1ce776e91ed
YIfan Bai <andybairoc@gmail.com>
2019-10-06 22:14:24 -0400

    hw2

--- assignment1/solution.py
+++ assignment1/solution.py
@@ -2,6 +2,7 @@ import numpy as np
 
 iris = np.loadtxt('iris.txt')
 
+
 ######## DO NOT MODIFY THIS FUNCTION ########
 def draw_rand_label(x, label_list):
     seed = abs(np.sum(x))
@@ -10,6 +11,8 @@ def draw_rand_label(x, label_list):
     seed = int(1000000 * seed)
     np.random.seed(seed)
     return np.random.choice(label_list)
+
+
 #############################################
 
 
@@ -34,7 +37,7 @@ class Q1:
             if i < len(iris):
                 result[i] = iris[num]
             i = i + 1
-        return np.transpose(np.mean(np.transpose(result[0:round(len(iris)/3)]), axis=1)[0:4])
+        return np.transpose(np.mean(np.transpose(result[0:round(len(iris) / 3)]), axis=1)[0:4])
         pass
 
     def covariance_matrix_class_1(self, iris):
@@ -45,29 +48,29 @@ class Q1:
             if i < len(iris):
                 result[i] = iris[num]
             i = i + 1
-        return np.cov(np.transpose(result[0:round(len(iris)/3)])[0:4])
+        return np.cov(np.transpose(result[0:round(len(iris) / 3)])[0:4])
         pass
-    
+
     def feature_means_class_1(self, iris):
         self.iris = iris
-        
+
         # cal the mask of is_class_1
         is_class_1 = iris[:, -1] == 1
-        
+
         # select data of class 1
         class_1_feature = iris[is_class_1, :-1]
-        
+
         return class_1_feature.mean(axis=0)
 
     def covariance_matrix_class_1(self, iris):
         self.iris = iris
-        
+
         # cal the mask of is_class_1
         is_class_1 = iris[:, -1] == 1
-        
+
         # select data of class 1
         class_1_feature = iris[is_class_1, :-1]
-        
+
         return np.cov(class_1_feature.T)
 
 
@@ -79,92 +82,89 @@ class HardParzen:
         self.label_list = label_list = np.unique(train_labels)
         self.train_data = train_inputs
         self.train_label = train_labels
-        
+
         return
-     
-    
+
     def is_in_the_window(self, center, inputs):
         def cal_euclidean_distance(x, y):
-#             print(np.sqrt(np.sum((x-y)**2, axis=-1)))
-            return np.sqrt(np.sum((x-y)**2, axis=-1))
-        
+            #             print(np.sqrt(np.sum((x-y)**2, axis=-1)))
+            return np.sqrt(np.sum((x - y) ** 2, axis=-1))
+
         # use euclidean distance as metric
         return cal_euclidean_distance(center, inputs) < self.h
-    
 
     def compute_predictions(self, test_data):
         predictions = []
         for te in test_data:
             # find neighbors of te
             is_neighbors = self.is_in_the_window(te, self.train_data)
-            
+
             if is_neighbors.sum() == 0:
                 predictions.append(int(draw_rand_label(te, self.label_list)))
                 continue
-            
+
             # get the label list of its neighbors
             neighbor_labels = self.train_label[is_neighbors]
-            
+
             # cal the counts of each class
             uniqued, counts = np.unique(neighbor_labels, return_counts=True)
-            
+
             # the prediction is the lable with largest count
             predictions.append(int(uniqued[counts.argmax()]))
-        
+
         return np.array(predictions)
-    
+
 
 class SoftRBFParzen:
     def __init__(self, sigma):
-        self.sigma  = sigma
+        self.sigma = sigma
 
     def train(self, train_inputs, train_labels):
         self.label_list = label_list = np.unique(train_labels)
         self.train_data = train_inputs
         self.train_label = train_labels
         self.dim = self.train_data.shape[-1]
-        
+
         return
 
     def compute_predictions(self, test_data):
         predictions = []
-        
+
         def cal_euclidean_distance(x, y):
-            return np.sqrt(np.sum((x-y)**2, axis=-1))
-        
+            return np.sqrt(np.sum((x - y) ** 2, axis=-1))
+
         # cal kernel density estimates
         def cal_kde(x, mean, sigma):
 
-            former = 1. / ( (2*np.pi)**(self.dim/2) * sigma**self.dim )
-            latter = np.exp( -0.5 * cal_euclidean_distance(x, mean)**2 / sigma**2 )
-            
+            former = 1. / ((2 * np.pi) ** (self.dim / 2) * sigma ** self.dim)
+            latter = np.exp(-0.5 * cal_euclidean_distance(x, mean) ** 2 / sigma ** 2)
+
             return former * latter
-        
+
         for te in test_data:
             weights = []
             for l in self.label_list:
                 # select all data of class l
-                data_l = self.train_data[self.train_label==l]
-                
+                data_l = self.train_data[self.train_label == l]
+
                 # cal the weight for all data_l
                 weight_l = cal_kde(data_l, mean=te, sigma=self.sigma)
-                
+
                 # add the total weight of class l to weights
                 weights.append(weight_l.sum())
-                
+
             # the current prediction is the label with largest weight
             predictions.append(int(self.label_list[np.argmax(weights)]))
-        
+
         assert predictions is not None
         return np.array(predictions)
-    
+
 
 def split_dataset(iris):
-
     training_set = np.zeros(shape=(len(iris), 5))
     validation_set = np.zeros(shape=(len(iris), 5))
-    test_set = np.zeros(shape=(len(iris), 5)) 
-    
+    test_set = np.zeros(shape=(len(iris), 5))
+
     j = 0;
     k = 0;
     l = 0;
@@ -178,7 +178,6 @@ def split_dataset(iris):
         elif i % 5 == 4:
             test_set[l] = iris[i]
             l += 1
-
 
     training_set = training_set[~np.all(training_set == 0, axis=1)]
     validation_set = validation_set[~np.all(validation_set == 0, axis=1)]
@@ -194,17 +193,17 @@ class ErrorRate:
         self.y_train = y_train
         self.x_val = x_val
         self.y_val = y_val
-    
+
     def cal_error_rate(self, pred, label):
-        return (pred.astype(int)!=label.astype(int)).sum() / label.shape[0]
-    
+        return (pred.astype(int) != label.astype(int)).sum() / label.shape[0]
+
     def hard_parzen(self, h):
         hp = HardParzen(h=h)
 
         hp.train(self.x_train, self.y_train)
 
         pred = hp.compute_predictions(self.x_val)
-        
+
         return self.cal_error_rate(pred, self.y_val)
 
     def soft_parzen(self, sigma):
@@ -213,36 +212,34 @@ class ErrorRate:
         sp.train(self.x_train, self.y_train)
 
         pred = sp.compute_predictions(self.x_val)
-        
+
         return self.cal_error_rate(pred, self.y_val)
 
 
 def get_test_errors(iris):
     hs = [0.001, 0.01, 0.1, 0.3, 1.0, 3.0, 10.0, 15.0, 20.0]
     sigmas = [0.001, 0.01, 0.1, 0.3, 1.0, 3.0, 10.0, 15.0, 20.0]
-    
+
     (training_set, validation_set, test_set) = split_dataset(iris)
-    
+
     tr_x, tr_y = training_set[:, :-1], training_set[:, -1]
     val_x, val_y = validation_set[:, :-1], validation_set[:, -1]
     te_x, te_y = test_set[:, :-1], test_set[:, -1]
-    
+
     er = ErrorRate(tr_x, tr_y, val_x, val_y)
-    
+
     er_hp = np.array([er.hard_parzen(h) for h in hs])
     er_sp = np.array([er.soft_parzen(sigma) for sigma in sigmas])
-    
-    
+
     h_star = hs[er_hp.argmin()]
     sigma_star = sigmas[er_sp.argmin()]
-    
+
     er_te = ErrorRate(tr_x, tr_y, te_x, te_y)
 
-    
     results = np.array([er_te.hard_parzen(h_star), er_te.soft_parzen(sigma_star)])
-    
+
     return results
-        
-    
+
+
 def random_projections(X, A):
-    return 1./2**0.5 * A.T.dot(X.T).T
+    return 1. / 2 ** 0.5 * A.T.dot(X.T).T
